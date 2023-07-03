e1fb2c10e6a5dea0a3583d9f7ef6a0d3de27d72e
saravana-ss2004 <saravanakumar.ss2004@gmail.com>
2023-07-03 18:49:27 +0530

    partially writen javadoc

--- Collections/LinkedList.java
+++ Collections/LinkedList.java
@@ -1,25 +1,55 @@
+/**
+ * LinkedList class creates head element for linkedlist.
+ * The head attribute has default value as null.
+ * 
+ * @author : Saravanakumar S S
+ * @Email  : saravanakumar.ss@zohocorp.com
+ * @since  : 03/07/2023
+ */
+
 public class LinkedList {
     Node head;
 
-    class Node{
-        int data;
-        Node next;
 
+    LinkedList(){
+        head = null;
+    } 
+
+
+    /*
+     * The Node inner class has two attributes int data and Node next.
+     * Node class creates node for linkedlist.
+     */
+    class Node{
+        int data; // The data attribute contains the actual data.
+        Node next; // The next attribute contains the next object reference.
+
+
+        /**
+         * Constructor for Node class
+         * @param val (value) parameter takes the data to store in Node.
+         */
         Node(int val){
             data = val;
             next = null;
         }
     }
 
-    LinkedList(){
-        head = null;
-    } 
-
+    
+    /**
+     * insert method set the data to the head element.
+     * @param val (value) takes the data.
+     */
     public void insert(int val){
-        Node newNode = new Node(val);
+        Node newNode = new Node(val); //Creates Node element with some data.
 
-        if(head==null){
-            head = newNode;
+        /**
+         * if   : assigns newNode to head
+         * else : head element is assigned to new node's next object reference and new node assigned to head element
+         */
+
+        if(head==null){ 
+            head = newNode; 
         }
         else{
             newNode.next = head;
@@ -28,24 +58,33 @@ public class LinkedList {
     }
 
 
+    /**
+     * The show used to display the elements
+     */
     public void show(){
         Node temp = head;
 
         while(temp != null){
             System.out.print(temp.data+" ");
-            temp = temp.next;
+            temp = temp.next; // jump to next element(object)
 
         }
         System.out.println();
     }
 
-    public void insertAtPos(int pos, int val){
-        if(pos == 0){
-            insert(val);
-            return;
+    /**
+     * The method insertAtposition used to insert an data in a specific location
+     * @param pos (position) index value for specific location
+     * @param val (value) takes the data
+     */
+
+    public void insertAtPosition(int pos, int val){
+        if(pos == 0){   // location 0 is similar to insert method. 
+            insert(val);  // So calling insert method
+            return;  // stop the function
         }
         
-        Node newNode = new Node(val);
+        Node newNode = new Node(val); // creating new node element
 
         Node temp = head;
         
